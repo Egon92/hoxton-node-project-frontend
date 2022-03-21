b160b8d37f3ff4d173cd447645def802e463131e
Egon <loliegon@gmail.com>
2022-03-21 11:52:57 +0100

    finished login page

--- src/Pages/Login-Page/Login-Page.tsx
+++ src/Pages/Login-Page/Login-Page.tsx
@@ -1,7 +1,39 @@
+import { Link } from "react-router-dom";
 import "./Login-Page.css";
 
 function Login() {
-  return <h1>This is a Login Page</h1>;
+  return (
+    <div className="login-page-wrapper">
+      <div className="left-main-wrapper">
+        <img
+          id="login-page-img"
+          src="https://images.pexels.com/photos/4050401/pexels-photo-4050401.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+          // src="https://images.pexels.com/photos/1851415/pexels-photo-1851415.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+          alt=""
+        />
+      </div>
+      <div className="right-main-wrapper">
+        <form id="login-form">
+          <h1>SocialLounge</h1>
+          <label htmlFor="">
+            <input type="text" placeholder="Enter your email" />
+          </label>
+          <label htmlFor="">
+            <input type="text" placeholder="Enter your password" />
+          </label>
+          <label htmlFor="">
+            <button>Log In</button>
+          </label>
+          <label id="signup-link-wrapper" htmlFor="">
+            Don't have an account?{" "}
+            <Link id="link" to={"../Signup"}>
+              Sign Up
+            </Link>
+          </label>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
