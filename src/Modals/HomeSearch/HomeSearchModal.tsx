2154d4eea40d8e12ee59c41fdc0b020c0b3fa1aa
Egon92 <90727274+Egon92@users.noreply.github.com>
2022-03-22 14:52:54 +0100

    Added photo modal and home page search modal

--- src/Modals/HomeSearch/HomeSearchModal.tsx
+++ src/Modals/HomeSearch/HomeSearchModal.tsx
@@ -1,11 +1,40 @@
+import "./HomeSearchModal.css";
+
 function HomeSearchModal() {
-  
-    return (
-    
-      <h1>This is Home search modal</h1>
-  
-    )
-    
-  }
-  
-  export default HomeSearchModal;
+  return (
+    <div id="homesearch-modal-wrapper">
+      <div id="top-modal">
+        <div id="recent-wrapper">
+          <span>Recent</span>
+        </div>
+        <div id="clear-all-wrapper">
+          <span>Clear all</span>
+        </div>
+      </div>
+      <div id="bottom-modal">
+        <div className="profile-wrapper">
+          <div className="profile-pic-wrapper">
+            <img
+              className="profile-pic"
+              src="../assets/icons/leva.jpg"
+              alt=""
+            />
+          </div>
+          <div className="name-username-wrapper">
+            <div className="name">Name Lastname</div>
+            <div className="username">Username</div>
+          </div>
+          <div className="x-wrapper">
+            <img
+              className="x-icon"
+              src="../assets/icons/close_black_24dp.svg"
+              alt=""
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default HomeSearchModal;
--- src/Modals/PhotoModal/PhotoModal.tsx
+++ src/Modals/PhotoModal/PhotoModal.tsx
@@ -1,11 +1,84 @@
+import "./PhotoModal.css";
 function PhotoModal() {
-  
-    return (
-    
-      <h1>This is Photo Modal</h1>
-  
-    )
-    
-  }
-  
-  export default PhotoModal;
+  return (
+    <div className="photo-modal-wrapper">
+      <div className="left-modal-wrapper">
+        <img id="modal-photo" src="../assets/icons/leva.jpg" alt="" />
+      </div>
+      <div className="right-modal-wrapper">
+        <div className="top-right-modal">
+          <div className="profile-name-wrapper">
+            <img id="profile-pic" src="../assets/icons/logo.jpg" alt="" />
+            <span id="name">Name Lastname</span>
+          </div>
+          <div className="content-wrapper">
+            <p id="description-text">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsa,
+              facere!
+            </p>
+            <span className="time-passed-since-post">2w</span>
+          </div>
+          <div className="three-points-wrapper">
+            <img
+              id="three-points-icon"
+              src="../assets/icons/more_horiz_black_24dp.svg"
+              alt=""
+            />
+          </div>
+        </div>
+        <div className="comments-wrapper">
+          <div className="comment-wrapper">
+            <span className="commentator">Name Lastname</span>
+            <p className="comment">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt
+              aut beatae ratione vero amet!
+            </p>
+          </div>
+        </div>
+        <div className="likes-and-icons-modal-wrapper">
+          <div className="icons-wrapper">
+            <img
+              id="like-icon"
+              src="../assets/icons/favorite_border_black_24dp.svg"
+              alt=""
+            />
+            <img
+              id="comment-icon"
+              src="../assets/icons/chat_bubble_outline_black_24dp.svg"
+              alt=""
+            />
+            <img
+              id="send-icon"
+              src="../assets/icons/send_black_24dp.svg"
+              alt=""
+            />
+          </div>
+          <div className="number-of-likes-wrapper">
+            <span id="number-of-likes">Liked by x people</span>
+          </div>
+        </div>
+        <div className="comment-and-number-wrapper">
+          <label className="emojis-wrapper" htmlFor="">
+            <img
+              id="emojis-icon"
+              src="../assets/icons/insert_emoticon_black_24dp.svg"
+              alt=""
+            />
+          </label>
+          <label id="comment-wrapper" htmlFor="">
+            <input
+              type="text"
+              id="comment-text"
+              placeholder="Enter your comment"
+            />
+          </label>
+          <label className="post-button" htmlFor="">
+            <button id="post-button">POST</button>
+          </label>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default PhotoModal;
