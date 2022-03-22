3b3685be9479d339c823ce126f93f2fa3e466763
Egon92 <90727274+Egon92@users.noreply.github.com>
2022-03-22 17:21:35 +0100

    Finished all modals

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import {
   Navigate,
 } from "react-router-dom";
 import "./App.css";
+import FollowerModal from "./Modals/FollowerModal/FolloweModal";
+import FollowingModal from "./Modals/FollowingModal/FollowingModal";
 import Error404 from "./Pages/Error/Error";
 import Homepage from "./Pages/Homepage/Homepage";
 import Login from "./Pages/Login-Page/Login-Page";
@@ -14,45 +16,83 @@ import Profile from "./Pages/Profile-Page/Profile-Page";
 import Signup from "./Pages/Signup-Page/Signup-Page";
 
 function App() {
+  const [user, setUser] = useState(null);
 
-  const [user, setUser] = useState(null)
-  
-  function validateUser () {
-
+  function validateUser() {
     if (localStorage.token) {
-        
-        fetch('http://localhost:4000/validate', {
-          headers: {
-              Authorization: localStorage.token
+      fetch("http://localhost:4000/validate", {
+        headers: {
+          Authorization: localStorage.token,
+        },
+      })
+        .then((resp) => resp.json())
+        .then((data) => {
+          if (data.error) {
+            console.log("Validation failed.");
+          } else {
+            setUser(data);
           }
-        })
-        .then(resp => resp.json())
-        .then(data => {
-
-            if (data.error) {
-              console.log('Validation failed.')
-            } 
-            
-            else {
-              setUser(data)
-            }
-
-        })
-
+        });
     }
-
   }
 
   return (
     <div className="App">
       <Routes>
         <Route index element={<Navigate replace to="/login" />} />
-        <Route path="/home" element={<Homepage user = {user} setUser = {setUser} validateUser = {validateUser} />} />
-        <Route path="/profile" element={<Profile user = {user} setUser = {setUser} validateUser = {validateUser} />} />
-        <Route path="/login" element={<Login  user = {user} setUser = {setUser} validateUser = {validateUser} />} />
-        <Route path="/signup" element={<Signup user = {user} setUser = {setUser} validateUser = {validateUser}/>} />
-        <Route path="/picture" element={<Picture user = {user} setUser = {setUser} validateUser = {validateUser} />} />
-        <Route path="*" element={<Error404 user = {user} setUser = {setUser} validateUser = {validateUser} />} />
+        <Route
+          path="/home"
+          element={
+            <Homepage
+              user={user}
+              setUser={setUser}
+              validateUser={validateUser}
+            />
+          }
+        />
+        <Route
+          path="/profile"
+          element={
+            <Profile
+              user={user}
+              setUser={setUser}
+              validateUser={validateUser}
+            />
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <Login user={user} setUser={setUser} validateUser={validateUser} />
+          }
+        />
+        <Route
+          path="/signup"
+          element={
+            <Signup user={user} setUser={setUser} validateUser={validateUser} />
+          }
+        />
+        <Route
+          path="/picture"
+          element={
+            <Picture
+              user={user}
+              setUser={setUser}
+              validateUser={validateUser}
+            />
+          }
+        />
+        <Route
+          path="*"
+          element={
+            <Error404
+              user={user}
+              setUser={setUser}
+              validateUser={validateUser}
+            />
+          }
+        />
+        <Route path="/following" element={<FollowingModal />} />
       </Routes>
     </div>
   );
